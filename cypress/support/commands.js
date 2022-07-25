d377c3e931ebd9923fa566f2f5caed35b5e9aeed
bwillenbring <bwillenbring@gmail.com>
2022-07-25 16:17:56 -0400

    Update commands.js
    - Add waitUntil and dayjs
    - Add 2 custom commands to main commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,18 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import 'cypress-wait-until'
+import dayjs from 'dayjs'
+
+Cypress.Commands.add('waitForPageToLoad', () => {
+    cy.waitUntil(() => cy.document().its('readyState').should('eq', 'complete'))
+    cy.log('Loaded!')
+})
+
+Cypress.Commands.add('getTimestamp', (date = dayjs()) => {
+    // Sun. 1 July, 2022
+    // See dayjs docs here: https://day.js.org/docs/en/display/format
+    const dateFormat = 'ddd. D MMMM, YYYY'
+    return dayjs(date).format(dateFormat)
+})
